61a506f4de6f3f51b3f87af69660c64b956adcf6
cytariel <cytariel@gmail.com>
2018-01-06 17:48:33 +0100

    Add files via upload

---
+++ js/app-gallery.js
@@ -0,0 +1,18 @@
+ $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        try {
+            var id = /-(\d+)$/.exec(id_selector)[1];
+            console.log(id_selector, id);
+            jQuery('#myCarousel').carousel(parseInt(id));
+        } catch (e) {
+            console.log('Regex failed!', e);
+        }
+    });
+
+$(window).scroll(function(){
+  var sticky = $('.sticky'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= 100) sticky.addClass('fixed');
+  else sticky.removeClass('fixed');
+});
---
+++ js/app.js
@@ -0,0 +1,15 @@
+//<!-- skrypt szybkosci zmiany slajdow w karuzeli -->
+$('.carousel').carousel({
+    interval: 100;
+})
+
+//    <!-- skrypt do określania aktywnego linku poprzez dodanie klasy active -->
+$(function () {
+    var current = location.pathname;
+    $('nav li a').each(function () {
+        var $this = $(this);
+        if ($this.attr('href').indexOf(current) !== -1) {
+            $this.addClass('active');
+        }
+    })
+})
