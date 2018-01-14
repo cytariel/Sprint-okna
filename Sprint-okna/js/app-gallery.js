57c4075c3679ea637d16973f78b8ba07ae9c1471
Aleksander Piron <33034685+aleksanderpiron@users.noreply.github.com>
2018-01-14 22:06:42 +0100

    Aktualna str

---
+++ Sprint-okna/js/app-gallery.js
@@ -0,0 +1,25 @@
+ $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        try {
+            var id = /-(\d+)$/.exec(id_selector)[1];
+            console.log(id_selector, id);
+            jQuery('#myCarousel').carousel(parseInt(id));
+        } catch (e) {
+            console.log('Regex failed!', e);
+        }
+    });
+
+$(window).scroll(function(){
+  var sticky = $('.sticky'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= 100) sticky.addClass('fixed');
+  else sticky.removeClass('fixed');
+});
+
+
+function fullscreen(){
+var current = $(".active").attr("id");
+	console.log(current);
+	$("#modal-img").html("<img class='img-fluid' src='../img/img-gallery-carousel/"+current+".jpg'>")
+}
