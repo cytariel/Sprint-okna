08fb71f73f2c918526b741cbc19520d1dbd20246
cytariel <cytariel@gmail.com>
2018-01-23 16:50:52 +0100

    nowosci

--- js/app.js
+++ js/app.js
@@ -26,20 +26,41 @@ $(window).scroll(function (event) {
 //group1
 $(".dropdown-toggle1").mouseover(
     function () {
-        $('.dropdown-menu1').slideDown('medium');
+        $('.dropdown-menu1').slideDown('fast');
     });
 
 $(".dropdown-group1").mouseleave(
     function () {
-        $('.dropdown-menu1').slideUp('medium');
+        $('.dropdown-menu1').slideUp('fast');
     });
 //group2
 $(".dropdown-toggle2").mouseover(
     function () {
-        $('.dropdown-menu2').slideDown('medium');
+        $('.dropdown-menu2').slideDown('fast');
     });
 
 $(".dropdown-group2").mouseleave(
     function () {
-        $('.dropdown-menu2').slideUp('medium');
+        $('.dropdown-menu2').slideUp('fast');
     });
+//skrypt walidacji formularza
+function formValidate() {
+    var id = "#" + $(event.target).attr('id');
+    if ($(id).val() == "") {
+        $(id).removeClass("correct");
+        $(id).addClass("wrong");
+        $(id).next().remove('.mark');
+        $(id).after('<i class="fas fa-times mark mark-wrong" ></i> <span class="mark mark-wrong comment">Pole musi być wypełnione!</span>');
+
+    } else if ($(id).val() != "") {
+        $(id).removeClass("wrong");
+        $(id).addClass("correct");
+        $(id).next().remove('.mark');
+        $(id).next().remove('.mark')
+        $(id).after('<i class="fas fa-check mark mark-ok"></i>');
+    }
+};
+$(function (){
+$("#phone").mask("999-999-999");
+$("#tele").mask("999-999-999");
+     })
